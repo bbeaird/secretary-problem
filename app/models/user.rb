bbe24720b4f32bc007e06f022cc98196bb79efbb
Brantley Beaird <bbeaird@gmail.com>
2014-01-20 14:20:53 -0800

    Finished sign in.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,23 @@
 include ActiveModel::ForbiddenAttributesProtection
 
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase }
+  before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
+
   attr_accessible :email, :name, :password, :password_confirmation
+
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
   has_secure_password
+
   validates :password, length: { minimum: 6 }, presence: true
   validates :password_confirmation, presence: true
+
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
