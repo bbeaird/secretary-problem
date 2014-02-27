4e3adc7cfa173c6e4ce14b9351654df3d6a84534
Brantley Beaird <bbeaird@gmail.com>
2014-02-26 18:13:56 -0800

    Added views for following and followers paths.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -83,78 +83,88 @@ describe "Authentication" do
           before { visit users_path }
           it { should have_title(full_title('Sign in')) }
         end
-      end
 
-      describe "when attempting to visit a protected page" do
-        before do
-          visit edit_user_path(user)
-          fill_in "Email",    with: user.email
-          fill_in "Password", with: user.password
-          click_button "Sign in"
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title(full_title('Sign in')) }
         end
 
-        describe "after signing in" do
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title(full_title('Sign in')) }
+        end
+      end
 
-          it "should render the desired protected page" do
-            page.should have_title(full_title('Edit user'))
+        describe "when attempting to visit a protected page" do
+          before do
+            visit edit_user_path(user)
+            fill_in "Email",    with: user.email
+            fill_in "Password", with: user.password
+            click_button "Sign in"
           end
 
-          describe "when signing in again" do
-            before do
-              delete signout_path
-              visit signin_path
-              fill_in "Email",    with: user.email
-              fill_in "Password", with: user.password
-              click_button "Sign in"
+          describe "after signing in" do
+
+            it "should render the desired protected page" do
+              page.should have_title(full_title('Edit user'))
             end
 
-            it "should render the default (profile) page" do
-              page.should have_title(full_title(user.name))
+            describe "when signing in again" do
+              before do
+                delete signout_path
+                visit signin_path
+                fill_in "Email",    with: user.email
+                fill_in "Password", with: user.password
+                click_button "Sign in"
+              end
+
+              it "should render the default (profile) page" do
+                page.should have_title(full_title(user.name))
+              end
             end
+          end
+        end
+
+        describe "in the Microposts controller" do
+
+          describe "submitting to the create action" do
+            before { post microposts_path }
+            specify { response.should redirect_to(signin_path) }
+          end
+
+          describe "submitting to the destroy action" do
+            before { delete micropost_path(FactoryGirl.create(:micropost)) }
+            specify { response.should redirect_to(signin_path) }
           end
         end
       end
 
-      describe "in the Microposts controller" do
+      describe "as wrong user" do
+        let(:user) { FactoryGirl.create(:user) }
+        let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+        before { sign_in user }
 
-        describe "submitting to the create action" do
-          before { post microposts_path }
-          specify { response.should redirect_to(signin_path) }
+        describe "visiting Users#edit page" do
+          before { visit edit_user_path(wrong_user) }
+          it { should_not have_selector('title', text: full_title('Edit user')) }
         end
 
-        describe "submitting to the destroy action" do
-          before { delete micropost_path(FactoryGirl.create(:micropost)) }
-          specify { response.should redirect_to(signin_path) }
+        describe "submitting a PUT request to the Users#update action" do
+          before { put user_path(wrong_user) }
+          specify { response.should redirect_to(root_url) }
         end
       end
-    end
 
-    describe "as wrong user" do
-      let(:user) { FactoryGirl.create(:user) }
-      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
-      before { sign_in user }
+      describe "as non-admin user" do
+        let(:user) { FactoryGirl.create(:user) }
+        let(:non_admin) { FactoryGirl.create(:user) }
 
-      describe "visiting Users#edit page" do
-        before { visit edit_user_path(wrong_user) }
-        it { should_not have_selector('title', text: full_title('Edit user')) }
-      end
+        before { sign_in non_admin }
 
-      describe "submitting a PUT request to the Users#update action" do
-        before { put user_path(wrong_user) }
-        specify { response.should redirect_to(root_url) }
-      end
-    end
-
-    describe "as non-admin user" do
-      let(:user) { FactoryGirl.create(:user) }
-      let(:non_admin) { FactoryGirl.create(:user) }
-
-      before { sign_in non_admin }
-
-      describe "submitting a DELETE request to the Users#destroy action" do
-        before { delete user_path(user) }
-        specify { response.should redirect_to(root_url) }
+        describe "submitting a DELETE request to the Users#destroy action" do
+          before { delete user_path(user) }
+          specify { response.should redirect_to(root_url) }
+        end
       end
     end
   end
-end
